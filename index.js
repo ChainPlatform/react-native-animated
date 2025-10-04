fe8385de13cc866c6b279bdb294e19396a9c26d7
haiidz <175676590+ChainPlatform@users.noreply.github.com>
2025-10-04 14:04:07 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-import DancingText from './src/DancingText';
+import { PureComponent } from 'react';
+import { Animated, View, Platform } from 'react-native';
 
-export default { DancingText }
+class DancingText extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.letters = typeof this.props.letters != "undefined" && this.props.letters != "" ? this.props.letters.split("") : "Chain".split("");
+        this.animations = this.letters.map(() => new Animated.Value(0));
+    }
+
+    componentDidMount() {
+        this.startAnimationSequence();
+    }
+
+    startAnimationSequence = () => {
+        const animations = this.animations.map((anim, index) =>
+            Animated.sequence([
+                Animated.timing(anim, {
+                    toValue: -5,
+                    duration: 200,
+                    useNativeDriver: Platform.OS === "web" ? false : true,
+                }),
+                Animated.timing(anim, {
+                    toValue: 5,
+                    duration: 200,
+                    useNativeDriver: Platform.OS === "web" ? false : true,
+                }),
+                Animated.timing(anim, {
+                    toValue: 0,
+                    duration: 200,
+                    useNativeDriver: Platform.OS === "web" ? false : true,
+                }),
+            ])
+        );
+        const staggeredAnimation = Animated.stagger(200, animations);
+        Animated.loop(staggeredAnimation).start();
+    };
+
+    render() {
+        return (
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                {this.letters.map((letter, index) => (
+                    <Animated.Text key={index} style={[typeof this.props.textStyle != "undefined" ? this.props.textStyle : {
+                        fontSize: 14,
+                        fontWeight: 'bold',
+                        color: '#4CAF50'
+                    }, {
+                        transform: [{
+                            translateY: this.animations[index]
+                        }]
+                    }
+                    ]}>{letter}</Animated.Text>
+                ))}
+            </View>
+        );
+    }
+}
+
+export default DancingText;
